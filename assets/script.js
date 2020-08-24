3e12336fc6c49a5063ab6f414f72805193b08f2a
Megan Sleezer <64713309+mslee001@users.noreply.github.com>
2020-08-23 21:31:28 -0700

    Added funcitionality to populate the output date field

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-    console.log("test");
+
     getNasaImg();
 
 //function to get the NASA Image of the day
@@ -18,31 +18,34 @@ $.ajax({
 })
 }
 
-//datepicker
-$("#date").datepicker({
-    altFormat: "@",
-    altField: "#unixDate"
-});
+//Moved the code below to the other "script.js" file
 
-//unix timestamp is saved as an altFormat to a hidden altField
-var altFormat = $("#date").datepicker("option", "altFormat");
-var altField = $("#unixDate").datepicker("option", "altField");
 
-$("#date").datepicker("option", "altFormat", "@");
-$("#unixDate").datepicker("option", "altField", "#unixDate");
+// //datepicker
+// $("#date").datepicker({
+//     altFormat: "@",
+//     altField: "#unixDate"
+// });
 
-//value to get the unix timestamp is: $("#unixDate").val();
+// //unix timestamp is saved as an altFormat to a hidden altField
+// var altFormat = $("#date").datepicker("option", "altFormat");
+// var altField = $("#unixDate").datepicker("option", "altField");
 
-//Google autocomplete for location
-var input = document.getElementById("location");
-var autocomplete = new google.maps.places.Autocomplete(input,{types: ['(cities)']});
-google.maps.event.addListener(autocomplete, 'place_changed', function(){
-    //place is an object that we can extract both city and state from
-    var place = autocomplete.getPlace();
-    console.log(place);
-    console.log(place.address_components[0].long_name);
-    console.log(place.address_components[2].short_name);
- })
+// $("#date").datepicker("option", "altFormat", "@");
+// $("#unixDate").datepicker("option", "altField", "#unixDate");
+
+// //value to get the unix timestamp is: $("#unixDate").val();
+
+// //Google autocomplete for location
+// var input = document.getElementById("location");
+// var autocomplete = new google.maps.places.Autocomplete(input,{types: ['(cities)']});
+// google.maps.event.addListener(autocomplete, 'place_changed', function(){
+//     //place is an object that we can extract both city and state from
+//     var place = autocomplete.getPlace();
+//     console.log(place);
+//     console.log(place.address_components[0].long_name);
+//     console.log(place.address_components[2].short_name);
+//  })
 
 
 
